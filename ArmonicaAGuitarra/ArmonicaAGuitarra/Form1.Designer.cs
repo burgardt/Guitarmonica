34ee73e21fbf4cefca162adc80b010ce102ba848
German Burgardt <geerman.b@gmail.com>
2015-11-22 05:35:13 -0300

    V0 - Funciona armonica a guitarra.

---
+++ ArmonicaAGuitarra/ArmonicaAGuitarra/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ArmonicaAGuitarra
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTab = new System.Windows.Forms.Label();
+            this.txtArmoAcord = new System.Windows.Forms.TextBox();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.btnNivel2 = new System.Windows.Forms.Button();
+            this.btnNivel1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTab
+            // 
+            this.lblTab.AutoSize = true;
+            this.lblTab.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTab.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTab.Location = new System.Drawing.Point(16, 114);
+            this.lblTab.Name = "lblTab";
+            this.lblTab.Size = new System.Drawing.Size(32, 24);
+            this.lblTab.TabIndex = 1;
+            this.lblTab.Text = "e-";
+            // 
+            // txtArmoAcord
+            // 
+            this.txtArmoAcord.Location = new System.Drawing.Point(180, 13);
+            this.txtArmoAcord.Multiline = true;
+            this.txtArmoAcord.Name = "txtArmoAcord";
+            this.txtArmoAcord.Size = new System.Drawing.Size(697, 17);
+            this.txtArmoAcord.TabIndex = 2;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(13, 13);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(161, 16);
+            this.lblTitulo.TabIndex = 3;
+            this.lblTitulo.Text = "Ingrese acordes armónica:";
+            // 
+            // btnNivel2
+            // 
+            this.btnNivel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNivel2.Location = new System.Drawing.Point(103, 54);
+            this.btnNivel2.Name = "btnNivel2";
+            this.btnNivel2.Size = new System.Drawing.Size(71, 43);
+            this.btnNivel2.TabIndex = 4;
+            this.btnNivel2.Text = "Nivel 2 [Agudo]";
+            this.btnNivel2.UseVisualStyleBackColor = true;
+            this.btnNivel2.Click += new System.EventHandler(this.btnNivel2_Click);
+            // 
+            // btnNivel1
+            // 
+            this.btnNivel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNivel1.Location = new System.Drawing.Point(16, 54);
+            this.btnNivel1.Name = "btnNivel1";
+            this.btnNivel1.Size = new System.Drawing.Size(81, 43);
+            this.btnNivel1.TabIndex = 5;
+            this.btnNivel1.Text = "Nivel 1\r\n[Grave]";
+            this.btnNivel1.UseVisualStyleBackColor = true;
+            this.btnNivel1.Click += new System.EventHandler(this.btnNivel1_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(889, 292);
+            this.Controls.Add(this.btnNivel1);
+            this.Controls.Add(this.btnNivel2);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.txtArmoAcord);
+            this.Controls.Add(this.lblTab);
+            this.Name = "frmPrincipal";
+            this.Text = "Armonica a Guitarra";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        //private System.Windows.Forms.Label lblPrueba;
+        private System.Windows.Forms.Label lblTab;
+        private System.Windows.Forms.TextBox txtArmoAcord;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button btnNivel2;
+        private System.Windows.Forms.Button btnNivel1;
+    }
+}
+
---
+++ ArmonicaAGuitarra/ArmonicaAGuitarra/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArmonicaAGuitarra
+{
+    public partial class frmPrincipal : Form
+    {
+
+        string cancionArmonica;
+        string[] tabs;
+        int[] posCuerdas;
+        int posNota;
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////        
+        public frmPrincipal()
+        {
+            InitializeComponent();
+            iniciarTab();
+        }
+
+        private void iniciarTab()
+        {
+            // Inicio los valores
+            tabs = new string[] { "e-", "B-", "G-", "D-", "A-", "E-" };
+            posCuerdas = new int[] { 2, 2, 2, 2, 2, 2 };
+            lblTab.Text = "e-\nB-\nG-\nD-\nA-\nE-";
+            posNota = 2;
+            
+        }
+
+        // Escribe en una cuerda una nota, poniendo guiones y eso automáticamente.
+        private void escribirNota(int nroCuerda, int nota)
+        {
+
+            tabs[nroCuerda] = tabs[nroCuerda] + nota;
+            
+            if (posNota != 2) // CON ESTE IF SE ARREGLA UN ERROR DE ESTÉTICA, SI SURGE ALGÚN PROBLEMA PUEDE QUE ESTÉ EN ESTA PARTE (PROBAR DEJARLO COMO ANTES).
+            {
+                if (nota < 10)
+                {
+                    tabs[nroCuerda] = tabs[nroCuerda] + "-";
+                    posCuerdas[nroCuerda] = posCuerdas[nroCuerda] + 2; //+2 porque pone nota y un guion
+                    posNota = posNota + 2;
+                }
+                else
+                {
+                    tabs[nroCuerda] = tabs[nroCuerda] + "-";
+                    posCuerdas[nroCuerda] = posCuerdas[nroCuerda] + 3; //+3 porque pone nota (q vale 2) y un guion
+                    posNota = posNota + 3;
+                }
+            }
+            else 
+            {
+                posCuerdas[nroCuerda] = posCuerdas[nroCuerda] + 1;
+                posNota = posNota + 1;
+            }
+            
+
+            // Acá se ponen los guiones
+            for (int i = 0; i<=5; i++)
+            {
+                while (posCuerdas[i] <= posNota)
+                { 
+                    tabs[i] = tabs[i] + "-";
+                    posCuerdas[i] = posCuerdas[i] + 1;
+                }
+            }
+          
+        }
+
+
+        private void traducir(int nivel)
+        {
+            // Reinicio los valores
+            iniciarTab();
+
+            // Separadores de la cadena
+            string[] separadores = new string[] {" "};
+            // Quito los espacios de mas
+            string auxCad = cancionArmonica.Trim(); 
+
+            for (int i = 1; i <= 10; i++)
+            { 
+                // Cambio los menos por parentesis
+                auxCad = auxCad.Replace("-"+i, "("+i+")");
+
+                // Agrego un espacio separador        
+                auxCad = auxCad.Replace(i + ")", i+") ");
+                auxCad = auxCad.Replace(i + "(", i + " (");
+
+                auxCad = auxCad.Replace(i + "" + i, i + " " + i);
+                for (int j = 1; j <= 10; j++)
+                {
+                    // Aca pongo un espacio entre medio de dos nros
+                    auxCad = auxCad.Replace(i + "" + j, i + " " + j);
+                    //auxCad = auxCad.Replace(j + "" + i, j + " " + i);
+                    
+                }
+
+
+            }
+
+            // En los nros > a 19 se desincroniza todo!
+            // HACER MULTILINEA FUNCIONAL Y ARREGLAR ERROR DE LOS GUIONES
+
+
+
+            foreach (string e in (auxCad.Split(separadores, System.StringSplitOptions.RemoveEmptyEntries)))
+            {
+                
+                switch (e)
+                {
+                    case "10":
+                        if (nivel==1)
+                            escribirNota(0, 8);
+                        if (nivel==2)
+                            escribirNota(0, 20);
+                        break;
+                    case "(10)":
+                        if (nivel==1)
+                            escribirNota(0, 5);
+                        if (nivel == 2)
+                            escribirNota(0, 17);
+                        break;
+                    case "9":
+                        if (nivel==1)
+                            escribirNota(0, 3);
+                        if (nivel == 2)
+                            escribirNota(0, 15);
+                        break;
+                    case "(9)":
+                        if (nivel==1)
+                            escribirNota(0, 1);
+                        if (nivel == 2)
+                            escribirNota(0, 13);
+                        break;
+                    case "8":
+                        if (nivel==1)
+                            escribirNota(0, 0);
+                        if (nivel == 2)
+                            escribirNota(0, 12);
+                        break;
+                    case "(8)":
+                        if (nivel==1)
+                            escribirNota(1, 3);
+                        if (nivel == 2)
+                            escribirNota(0, 10);
+                        break;
+                    case "7":
+                        if (nivel==1)
+                            escribirNota(1, 1);
+                        if (nivel == 2)
+                            escribirNota(0, 8);
+                        break;
+                    case "(7)":
+                        if (nivel==1)
+                            escribirNota(1, 0);
+                        if (nivel == 2)
+                            escribirNota(0, 7);
+                        break;
+                    case "6":
+                        if (nivel==1)
+                            escribirNota(2, 0);
+                        if (nivel == 2)
+                            escribirNota(0, 3);
+                        break;
+                    case "(6)":
+                        if (nivel==1)
+                            escribirNota(2, 2);
+                        if (nivel == 2)
+                            escribirNota(0, 5);
+                        break;
+                    case "5":
+                        if (nivel==1)
+                            escribirNota(3, 2);
+                        if (nivel == 2)
+                            escribirNota(0, 0);
+                        break;
+                    case "(5)":
+                        if (nivel==1)
+                            escribirNota(3, 3);
+                        if (nivel == 2)
+                            escribirNota(0, 1);
+                        break;
+                    case "4":
+                        if (nivel==1)
+                            escribirNota(4, 3);
+                        if (nivel == 2)
+                            escribirNota(1, 1);
+                        break;
+                    case "(4)":
+                        if (nivel==1)
+                            escribirNota(4, 5);
+                        if (nivel == 2)
+                            escribirNota(1, 3);
+                        break;
+                    case "3":
+                        if (nivel==1)
+                            escribirNota(5, 3);
+                        if (nivel == 2)
+                            escribirNota(2, 0);
+                        break;
+                    case "(3)":
+                        if (nivel==1)
+                            escribirNota(5, 7);
+                        if (nivel == 2)
+                            escribirNota(1, 0);
+                        break;
+                    case "2":
+                        if (nivel == 2)
+                            escribirNota(3, 2);
+                        break;
+                    case "(2)":
+                        if (nivel == 2)
+                            escribirNota(3, 5);
+                        break;
+                    case "1":
+                        if (nivel == 2)
+                            escribirNota(4, 3);
+                        break;
+                    case "(1)":
+                        if (nivel == 2)
+                            escribirNota(4, 5);
+                        break;
+                    default:
+                        if (e != "")
+                            MessageBox.Show("Esta parte esta mal escrita: " + e);
+                        break;
+                }
+            }
+
+            // Transpaso el array de la tab al label
+            lblTab.Text = tabs[0] + "\n" + tabs[1] + "\n" + tabs[2] + "\n" + tabs[3] + "\n" + tabs[4] + "\n" + tabs[5];
+
+            
+        }
+
+       
+        private void btnNivel2_Click(object sender, EventArgs e)
+        {
+            cancionArmonica = txtArmoAcord.Text;
+            traducir(2);//Lo llamo a nivel 2, mas agudo
+        }
+
+        private void btnNivel1_Click(object sender, EventArgs e)
+        {
+            cancionArmonica = txtArmoAcord.Text;
+            traducir(1);//Lo llamo a nivel 1, mas grave
+        }
+
+    }
+}
